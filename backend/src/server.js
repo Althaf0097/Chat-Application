1c9108a3971b79600466685fdf53c6ab6db8787d
Althaf0097 <althafs879@gmail.com>
2025-12-25 12:43:51 +0530

    engines field added mongodb.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ import path from "path";
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import { connectDB } from './lib/db.js';
+
 
 dotenv.config();
 
@@ -23,6 +25,7 @@ if(process.env.NODE_ENV === "production"){
     });
 }
 
-app.listen(PORT, () => 
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
-)
+    connectDB();
+})
