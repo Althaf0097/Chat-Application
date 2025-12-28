26cba725b11dd5614e91e73c5d827906af439a1c
Althaf0097 <althafs879@gmail.com>
2025-12-28 11:01:22 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,6 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 
-
 dotenv.config();
 
 const app = express();
@@ -14,8 +13,11 @@ const _dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());  //req.body parser
+
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+
 // make ready for deployment
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(_dirname,"../frontend/dist")));
